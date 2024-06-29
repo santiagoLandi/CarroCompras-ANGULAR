4e33b7e93b98ca8ec6c6b2dd0f98a94546b01e7a
Santiago Landi <slandi1992@gmail.com>
2024-06-29 11:45:19 -0300

    se hizo el ruteo del sitio y se utlizo directiva routerlink para los link del nav

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PescaTiendaComponent } from './pesca-tienda/pesca-tienda.component';
+import { PescaAboutComponent } from './pesca-about/pesca-about.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',//path raiz
+    redirectTo: 'compras',
+    pathMatch: 'full'
+  },
+  {
+    path: 'compras',
+    component: PescaTiendaComponent
+  },
+  {
+    path: 'about',
+    component: PescaAboutComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
